59ea705818bb1dece47a959641fdd62bc28f9d1e
Daniel Rhodes <danielr@outlook.co.nz>
2018-06-01 18:08:37 +1200

    Updated Js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,4 @@
 
-
-// User clicked on the Add Button
-// Add text inside item field to "To Do List"
-document.getElementById('add').addEventListener('click', function() {
-  var value = document.getElementById('item').value;
-  if (value) addItemTodo(value);
-});
-
-function removeItem() {
-  console.log(this);
-
-}
-
 // User clicked on the Add Button
 // Add text inside item field to "To Do List"
 document.getElementById('add').addEventListener('click', function() {
@@ -21,47 +8,64 @@ document.getElementById('add').addEventListener('click', function() {
 
 // Function to remove item
 function removeItem() {
- console.log(this);
+  var item = this.parentNode.parentNode;
+  var parent = item.parentNode;
+
+  parent.removeChild(item);
+
 }
 
 //Function to check item
 function checkItem() {
- console.log(this);
+  var item = this.parentNode.parentNode;
+  var parent = item.parentNode;
+  var id = parent.id;
+
+  //check if the item should be check or re-added to todo
+  var target = (id === 'todo') ? document.getElementById('checked'): document.getElementById('todo');
+
+  parent.removeChild(item);
+  target.insertBefore(item, target.childNodes[0]);
+
 }
 
-
-
-//
+//Adds item to List
 function addItemTodo(text) {
 
+// grabbing ordered list
  var list = document.getElementById('todo');
 
+// Creating list item
  var item = document.createElement('li');
  item.innerText = text;
 
+ // Creating div for buttons
  var buttons = document.createElement('DIV');
  buttons.classList.add('buttons');
 
+
+// Remove item
  var remove = document.createElement('BUTTON');
  var t = document.createTextNode("Delete");
  remove.appendChild(t);
  document.body.appendChild(remove);
 
-
- // Remove Item
+ //Adding click event for removing item
  remove.addEventListener('click', removeItem);
 
+
+ // Check item
  var check = document.createElement('BUTTON');
  var t = document.createTextNode("Check");
  check.appendChild(t);
  document.body.appendChild(check);
 
-
- // Check Item
+ //Adding click event for checking item
  check.addEventListener('click', checkItem);
 
+ // Adding buttons to Parent
  buttons.appendChild(remove);
- buttons.appendChild(checked);
+ buttons.appendChild(check);
  item.appendChild(buttons);
 
  list.insertBefore(item, list.childNodes[0]);
